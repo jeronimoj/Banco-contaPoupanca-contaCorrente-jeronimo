f91db573f3c25eec03c7d4af466f690e7cbdef58
Jerónimo Paulo Pinto <dev4life228@gmail.com>
2022-03-07 00:32:01 +0100

    Banco Digital feito

--- src/jeronimo/com/Main.java
+++ src/jeronimo/com/Main.java
@@ -4,20 +4,21 @@ public class Main {
     public static void main(String[] args){
       /*Conta normal*/
         Conta conta = new Conta();
+        Conta conta1 = new Conta();
 
         conta.setNomeCliente("Jerónimo Paulo Pinto");
         conta.setNumeroConta("000023454b0");
         conta.setSaldo(300);
 
-        conta.depositar(200.4);
+        conta.depositar(200);
 
-        if(conta.sacar(23.5)){
+        if(conta.sacar(30)){
             System.out.println("Sacou com sucesso, novo saldo"+conta.getSaldo());
         }else{
             System.out.println("Sem sucesso,");
 
         }
-        if(conta.transferencia(conta, 200)){
+        if(conta.transferencia(conta1, 200)){
             System.out.println("Transferencia feita com sucesso, novo saldo"+conta.getSaldo());
         }else{
             System.out.println("Transferencia sem sucesso,");
@@ -30,20 +31,22 @@ public class Main {
         System.out.println("--------------Conta Corrente---------------");
         ContaCorrente contaCorrente = new ContaCorrente();
         contaCorrente.setNumeroConta("234254354556");
+        contaCorrente.setSaldo(200);
         contaCorrente.setLimite(300);
 
+        if(contaCorrente.sacar(23.5)){
+            System.out.println("Sacou com sucesso, novo saldo"+contaCorrente.getSaldo());
+        }else{
+            System.out.println("Sem sucesso,");
+        }
         if(contaCorrente.sacar2(20)){
             System.out.println("Sacou com sucesso, novo saldo"+contaCorrente.getSaldo());
         }else{
             System.out.println("Sem sucesso, excedeu o limite");
         }
         contaCorrente.depositar(200.4);
-        if(contaCorrente.sacar(23.5)){
-            System.out.println("Sacou com sucesso, novo saldo"+contaCorrente.getSaldo());
-        }else{
-            System.out.println("Sem sucesso,");
-        }
-        if(contaCorrente.transferencia(conta, 200)){
+
+        if(contaCorrente.transferencia(conta1, 200)){
             System.out.println("Transferencia feita com sucesso, novo saldo"+contaCorrente.getSaldo());
         }else{
             System.out.println("Transferencia sem sucesso,");
@@ -56,6 +59,7 @@ public class Main {
         ContaPoupanca contaPoupanca = new ContaPoupanca();
         contaPoupanca.setNomeCliente("Jerónimo Pinto");
         contaPoupanca.setNumeroConta("234254354556");
+        contaPoupanca.setSaldo(400);
         contaPoupanca.setDiaRedimento(6);
 
         System.out.println( contaPoupanca.calculoRendimento(0.3));
@@ -65,7 +69,7 @@ public class Main {
         }else{
             System.out.println("Sem sucesso,");
         }
-        if(contaPoupanca.transferencia(conta, 200)){
+        if(contaPoupanca.transferencia(conta1, 200)){
             System.out.println("Transferencia feita com sucesso, novo saldo"+contaPoupanca.getSaldo());
         }else{
             System.out.println("Transferencia sem sucesso,");
